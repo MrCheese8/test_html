b11809accc0e2c870cabc059a7bb8f879254bdbc
MrCheese8 <161657091+MrCheese8@users.noreply.github.com>
2024-09-25 21:32:36 +0000

    Stuff

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 //TASK 2
+let box1 = document.querySelector("#box1");
+let box2 = document.querySelector("#box2");
+let box3 = document.querySelector("#box3");
+let box4 = document.querySelector("#box4");
+let box5 = document.querySelector("#box5");
 
+box1.classList.add('pink');
 
-
+let makePink = 
