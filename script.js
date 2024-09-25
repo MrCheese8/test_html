e5aa6001715a9332ad06690bb2942e649805e56d
MrCheese8 <161657091+MrCheese8@users.noreply.github.com>
2024-09-25 22:34:19 +0000

    bruh

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ let box3 = document.querySelector("#box3");
 let box4 = document.querySelector("#box4");
 let box5 = document.querySelector("#box5");
 let box6 = document.querySelector("#box6");
+let box7 = document.querySelector("#box7");
 
 //function that turns box1 pink
 let makePink = () => {
